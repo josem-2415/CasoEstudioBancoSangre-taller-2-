f9ba5ca0448b00ce5b137f43ae6ab7cfdb744ed8
Nerie <vasqueznerie918@gmail.com>
2025-09-30 18:36:08 -0500

    Update BloodDatabase.h

--- BloodDatabase.h
+++ BloodDatabase.h
@@ -1,3 +1,24 @@
+/* 
+Clase: BloodDataBase
+Curso: Fundamanetos de programacion orientada a objetos (FPOO)
+UNIVERSIDAD EL VALLE
+informacion CRC:
+Responsabilidades:
+-Gestionar y administrar la base de datos completa de donantes de sangre, incluyendo operaciones de creación,
+lectura, actualización y eliminación de registros.
+-Mostrar interfaz de usuario para permitir la intereccion de la perosna con el sistema, donde le muestra el menu
+, las opciones y los resultados.
+-Manejar la resitencia de datos en archivos, garantizando que la información se almacene y recupere correctamente.
+-Validar y procesar entradas del usuario, asegurando la integridad y corrección de los datos ingresados.
+-Coordinar las operaciones del sistema entre las diferentes funcionalidades y la interfaz de usuario.
+
+Colaboradores:
+-Colabora con la clase Donor, que representa a cada donante individual y proporciona la estructura de datos para almacenar la información.
+-Interactúa con std::vector para gestionar colecciones de objetos Donor en memoria.
+-Utiliza std::fstream para operaciones de lectura y escritura en archivos de persistencia.
+-Colabora con std::cin y std::cout para la interacción con el usuario mediante consola.
+
+*/
 #ifndef BLOODDATABASE_H
 #define BLOODDATABASE_H
 
